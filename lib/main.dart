425b901f34dd89cf7be54fab54b33d5486ef2db2
Basmala Ashraf <2200410@student.eelu.edu.eg>
2025-12-05 19:45:27 +0200

    start auth

--- lib/main.dart
+++ lib/main.dart
@@ -5,17 +5,25 @@ import 'features/splash/presentation/pages/splash_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+class _MyAppState extends State<MyApp> {
   @override
+  void initState() {
+    FirebaseAuth.instance.authStateChanges().listen((User? user) {
+      if (user == null) {
+        print('User is currently signed out!');
+      } else {
+        print('User is signed in!');
+      }
+    });
+    super.initState();
+  }
+    @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: AppConstants.appName,
@@ -24,4 +32,12 @@ class MyApp extends StatelessWidget {
       home: const SplashPage(),
     );
   }
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+  @override
+  State<MyApp> createState() => _MyAppState();
+
+
 }
